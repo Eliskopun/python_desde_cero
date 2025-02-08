9033b2cedf84618fea3b6a80c8a1a25981a55a5a
Eliskopun <eliezeravilan@gmail.com>
2025-02-08 14:34:29 -0500

    [ADD] 0010_helloworld_exercise.py con comentarios explicativos del Ejercicio 9

---
+++ ejercicios/divididos_por_leccion/0010_helloworld_exercise.py
@@ -0,0 +1,3 @@
+# 10. Comenta el código del Ejercicio 9, y explica qué hace cada línea usando comentarios de una sola línea.
+# imprimir la suma 28 mas 11, su resultado es igual 39
+# imprimir el tipo de dato de la suma entre 28 mas 11, su resultado es tipo int (entero)
