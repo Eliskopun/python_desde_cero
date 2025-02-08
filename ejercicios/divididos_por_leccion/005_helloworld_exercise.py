eed4ffe3638660b84da7c37bb089e8e892171219
Eliskopun <eliezeravilan@gmail.com>
2025-02-08 14:31:33 -0500

    [ADD] 005_helloworld_exercise.py con explicación de los tipos de datos en Python

---
+++ ejercicios/divididos_por_leccion/005_helloworld_exercise.py
@@ -0,0 +1,10 @@
+# 5. Escribe un comentario en varias líneas explicando qué son los tipos de datos en Python.
+"""
+los tipos de datos de python
+son categorias que nos permiten
+diferenciar su caracteristica 
+principal del dato
+los mas comunes son: texto, 
+numeros enteros, numeros decimales 
+y numeros complejos
+"""
