d7bfc9a5ea9c91362ffe717bf23c896a1a042365
Eliskopun <eliezeravilan@gmail.com>
2025-02-08 14:30:21 -0500

    [ADD] 004_helloworld_exercise.py con impresión de tipos de datos básicos

---
+++ ejercicios/divididos_por_leccion/004_helloworld_exercise.py
@@ -0,0 +1,4 @@
+# 4. Usa la función type() para imprimir el tipo de dato de una cadena de texto, un número entero y un número decimal.
+print(type("¡Hola comunidad!"))
+print(type(28))
+print(type(1.2))
