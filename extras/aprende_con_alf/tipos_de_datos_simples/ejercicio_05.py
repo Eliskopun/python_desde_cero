e730af37e5bed4b4d3ddf5ade1a061498ad2a5ac
Eliskopun <eliezeravilan@gmail.com>
2025-02-09 22:35:06 -0500

    [ADD] Ejercicio 5: Calcula la paga del usuario según horas trabajadas y coste por hora

---
+++ extras/aprende_con_alf/tipos_de_datos_simples/ejercicio_05.py
@@ -0,0 +1,5 @@
+# Ejercicio 5:
+# Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+horas_trabajadas = int(input("Ingrese cuantas horas tiene trabajadas: "))
+coste_por_hora  = int(input("Ingrese cuanto es el coste por hora: "))
+print("La paga correspondiente es de: ",horas_trabajadas * coste_por_hora)
